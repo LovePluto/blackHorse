46739c21b652fd72411c7e66899456c9427bdd43
Pluto <591712730@qq.com>
2022-03-13 18:37:34 +0800

    [1|AC3|工序6]：完成调用航班服务出现异常状态处理

--- src/main/java/com/wyh/dark_horse/infrastructure/client/FlightClientImpl.java
+++ src/main/java/com/wyh/dark_horse/infrastructure/client/FlightClientImpl.java
@@ -5,6 +5,7 @@ import com.wyh.dark_horse.bookticket.model.BookResult;
 import com.wyh.dark_horse.bookticket.model.Ticket;
 import com.wyh.dark_horse.infrastructure.client.feignclient.FlightFeignClient;
 import com.wyh.dark_horse.infrastructure.client.request.TicketRequest;
+import com.wyh.dark_horse.infrastructure.exception.ThirdException;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -16,6 +17,10 @@ public class FlightClientImpl implements FlightClient {
 
     @Override
     public BookResult bookTicket(Ticket ticket) {
-        return flightFeignClient.bookTicket(TicketRequest.from(ticket));
+        try {
+            return flightFeignClient.bookTicket(TicketRequest.from(ticket));
+        } catch (Exception e) {
+            throw new ThirdException("预订机票失败，请稍后重试或拨打电话 123456");
+        }
     }
 }
---
+++ src/main/java/com/wyh/dark_horse/infrastructure/exception/ThirdException.java
@@ -0,0 +1,9 @@
+package com.wyh.dark_horse.infrastructure.exception;
+
+public class ThirdException extends RuntimeException {
+
+    public ThirdException(String message) {
+        super(message);
+    }
+
+}
